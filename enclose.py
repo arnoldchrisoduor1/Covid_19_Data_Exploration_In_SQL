25cfd395cc684218ceb775655e20b9605fb8444f
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-09-13 03:20:50 +0300

    separate each row with a bracket and a comma

--- enclose.py
+++ enclose.py
@@ -1,15 +1,17 @@
 import csv
 
-def enclose_values_in_single_quotes(input_file):
+def enclose_values_in_brackets(input_file):
     try:
         with open(input_file, 'r') as csvfile:
             reader = csv.reader(csvfile)
+            rows = []
             for row in reader:
                 quoted_values = [f"'{value}'" for value in row]
-                print(', '.join(quoted_values) + ',')  # Add a comma at the end of each row
+                rows.append('('.join(quoted_values) + ')')  # Enclose each row in brackets
+            print(',\n'.join(rows))  # Separate multiple rows with a comma and a newline
     except FileNotFoundError:
         print(f"The file '{input_file}' was not found.")
 
 if __name__ == "__main__":
     input_file = input("Enter the name of the CSV file: ")
-    enclose_values_in_single_quotes(input_file)
+    enclose_values_in_brackets(input_file)
